a83c361f50647abccef48629fdcaa4820700b339
xia0m <alexma325@gmail.com>
2017-10-23 17:34:46 -0700

    SearchBooks Cleanup

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -55,29 +55,7 @@ class SearchBooks extends Component {
                 
                     <ChangeShelf shelfBooks={results} 
                                  onUpdateBook={this.props.onUpdateBook}/>
-                    {/* <ol className="books-grid">
-                    {results.map((book)=>(                                       
-                                        <li key={book.id}>
-                                            <div className="book">
-                                                <div className="book-top">
-                                                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
-                                                    <div className="book-shelf-changer">
-                                                        <select value={book.shelf} onChange={(event,aBook)=>this.handleChange(event,book)}>
-                                                            <option value="none" disabled>Move to...</option>
-                                                            <option value="currentlyReading">Currently Reading</option>
-                                                            <option value="wantToRead">Want to Read</option>
-                                                            <option value="read">Read</option>
-                                                            <option value="none">None</option>
-                                                        </select>
-                                                     </div>
-                                                    
-                                              </div>
-                                                    <div className="book-title">{book.title}</div>
-                                                    <div className="book-authors">{book.author}</div>
-                                                </div>                                          
-                                        </li>
-                                    ))}
-                    </ol> */}
+                    
                     </div>
                 </div>
             </div>
